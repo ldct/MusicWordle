ba0285b53c0be4f6978433cfb9a28e7d2ae5b538
Li Xuanji <xuanji@gmail.com>
2022-01-25 16:43:33 +0800

    sharing

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,15 +1,40 @@
 import { getGuessStatuses } from './statuses'
 import { solutionIndex } from './words'
 
+/* eslint-disable */
+function getParameterByName(name: string, url = window.location.href): string {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) {
+    return "";
+  }
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function sanitize(name: string): string {
+  let check = name.toLowerCase();
+  for (let trigger of ["cp", "god", "xj"]) {
+    if (check.includes(trigger)) {
+      return "kevin";
+    }
+  }
+  return name;
+}
+
 export const shareStatus = (guesses: string[]) => {
+  var shareString = `NotWordle ${solutionIndex} ${guesses.length}/6`;
+
+  let author = getParameterByName("author");
+  if (author.length > 0) {
+    shareString += `\nauthor: ${sanitize(author)}`
+  }
+
+  shareString += `\n\n ${generateEmojiGrid(guesses)}`
   navigator.clipboard.writeText(
-    'NotWordle ' +
-      solutionIndex +
-      ' ' +
-      guesses.length +
-      '/6\n\n' +
-      generateEmojiGrid(guesses)
-  )
+    shareString
+  );
 }
 
 export const generateEmojiGrid = (guesses: string[]) => {
